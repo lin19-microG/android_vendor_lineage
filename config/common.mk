61b39e8dd143f64f60b4ced10c89843a9973458d
MSe <mse1969@posteo.de>
2022-10-31 19:46:29 +0100

    config: Add microG shipped packages
    Change-Id: Ib5119dce0eda60d936169e10a45ef9032d9a8fce

--- config/common.mk
+++ config/common.mk
@@ -17,6 +17,15 @@ endif
 # of microG as location provider
 PRODUCT_PACKAGE_OVERLAYS := vendor/lineage/overlay/microg
 
+# MicroG packages
+PRODUCT_PACKAGES += \
+	GmsCore \
+	GsfProxy \
+	FakeStore \
+	IchnaeaNlpBackend \
+	NominatimGeocoderBackend \
+	additional_repos.xml
+
 ifeq ($(TARGET_BUILD_VARIANT),eng)
 # Disable ADB authentication
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=0
